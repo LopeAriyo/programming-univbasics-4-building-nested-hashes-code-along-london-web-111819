e796463a341e8fd7ef24ddff5a47c11ebdf4ac70
Lopè Ariyo <lopeariyo@gmail.com>
2019-11-01 21:23:07 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -8,31 +8,31 @@ def third_challenge
         },
       :matriarch => {
         #your key/value pairs here
-        name:
-        age:
+        name: ""
+        age: ""
         },
       :hero => {
         #your key/value pairs here
-        name:
-        age:
+        name: ""
+        age: ""
         },
       :hero_friends => []
    },
    :capulet => {
       :patriarch => {
         #your key/value pairs here
-        name:
-        age:
+        name: ""
+        age: ""
         },
       :matriarch => {
         #your key/value pairs here
-        name:
-        age:
+        name: ""
+        age: ""
         },
       :heroine => {
         #your key/value pairs here
-        name:
-        age:
+        name: ""
+        age: ""
         },
       :heroine_friends => []
    }
