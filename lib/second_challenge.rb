7072c6a3360059c8862757d7951b13c38a620814
Lopè Ariyo <lopeariyo@gmail.com>
2019-11-01 21:18:49 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,10 +2,17 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       #your key/value pairs here!
-      patriarch
+      :patriach => {},
+      :matriach => {},
+      :heroine => {}, 
+      :heroine_friends => []
       },
     :capulet => {
       #your key/value pairs here!
+      :patriach => {},
+      :matriach => {},
+      :heroine => {}, 
+      :heroine_friends => []
       }}
 
 
