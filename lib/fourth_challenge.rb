810525052a3d761a73ce7d82c2e63b12cc792926
Lopè Ariyo <lopeariyo@gmail.com>
2019-11-01 21:28:28 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -9,19 +9,19 @@ def fourth_challenge
         {name: "Benvolio", age: "17", attitude: "worried"},
         {name: "Steven", age: "30", attitude: "confused"}
       ]
-   }, 
+   },
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         #your hashes here!
-        {name: "Mercutio", age: "18", attitude: "hot-headed"}, 
+        {name: "Mercutio", age: "18", attitude: "hot-headed"},
         {name: "Nurse", age: "44", attitude: "worried"}
       ]
    }
 }
 
-  
+
 
 end
